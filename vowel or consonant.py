50132cdc85874a8b79f76fdf8d1a1eb863eecdea
rasheedsuhail <47409891+rasheedsuhail@users.noreply.github.com>
2019-02-07 12:18:31 +0530

    Update vowel or consonant.py

--- vowel or consonant.py
+++ vowel or consonant.py
@@ -1,6 +1,6 @@
-ch = input()
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+ht = input()
+if(ht=='A' or ht=='a' or ht=='E' or ht =='e' or ht=='I'
+ or ht=='i' or ht=='O' or ht=='o' or ht=='U' or ht=='u'):
     print("Vowel")
 else:
     print("Consonant")
