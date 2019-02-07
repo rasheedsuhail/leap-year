66e868351f427e2b3f7d71a50237a2b14d199857
rasheedsuhail <47409891+rasheedsuhail@users.noreply.github.com>
2019-02-07 12:05:12 +0530

    Create odd or even.py

---
+++ odd or even.py
@@ -0,0 +1,6 @@
+#Even Odd Program using Bitwise Operator  
+a=int(input("Please Enter a Number : "));  
+if(a&1==1):  
+    printf("This Number is Even")  
+else:  
+    printf("This Number is Odd")  
